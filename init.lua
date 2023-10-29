380b8601b2ae0ad09187e8d7afcdc6fd9c9b622d
Timo <timoisdepsky@gmail.com>
2023-10-29 22:14:39 +0100

    Revert "Change cmp snippet confirm from <Tab> to <Enter>"
    This reverts commit a73f8eb210236d422e621735d19478be2f64476f.

--- init.lua
+++ init.lua
@@ -79,7 +79,7 @@ vim.cmd([[colorscheme gruvbox]])
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<Enter>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
